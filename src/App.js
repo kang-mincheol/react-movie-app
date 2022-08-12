c86feb35057f358b85751afca6dde4c4bff41de3
mincheol <aaa6315@gmail.com>
2022-08-12 17:59:19 +0900

    useEffect 스터디

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setValue(prev => prev + 1);
-  console.log('call an api');
+  const onChange = (event) => setKeyword(event.target.value); 
+  
+  console.log('i run all time');
+  const iRunOnlyOne = () => {
+    console.log('i run only once');
+  }
+  useEffect(() => {
+    console.log("call the api..");
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5) {
+      console.log("search for", keyword);
+    }
+  }, [keyword]);
+
+  
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
