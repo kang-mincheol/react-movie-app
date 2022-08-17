6ae395f1a607d38e529bffd650a2d88dcd7e5c18
mincheol <aaa6315@gmail.com>
2022-08-17 18:36:54 +0900

    parameter get handling

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,30 @@
+import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-    return <h1>Detail</h1>
+    const { id } = useParams();
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [start, setStart] = useState('');
+    const getMovie = async() => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        console.log(json);
+        setTitle(json.data.movie.title);
+        setSummary(json.data.movie.description_intro);
+        setStart(json.data.movie.year);
+    }
+    useEffect(() => {
+        getMovie();
+    }, []);
+
+    return <div>
+        <h1>{title}</h1>
+        <p>{summary}</p>
+        <p>{start}</p>
+    </div>;
 }
 
 export default Detail;
